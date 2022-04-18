6ea3206ccf709e94e1c21638cc05f97ab3b2d1f9
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-04-18 18:47:59 +0530

    Context api Cart

---
+++ src/data.js
@@ -0,0 +1,87 @@
+const list = [
+    {
+      id: 1,
+      title: "Mobiles",
+      "name": "Mi 11 Ultra 5G",
+      "price": "₹ 69,999",
+      "image": "https://i02.appmifile.com/867_operator_in/23/04/2021/3849c44ac78d65621750a114811711f9.png?width=140&height=140"
+    },
+    {
+      id: 2,
+      title: "Mobiles",
+      "name": "Mi 11X Pro 5G",
+      "price": "from ₹ 39,999",
+      "image": "https://i02.appmifile.com/52_operator_in/04/03/2021/7db1136dfdc2a1e9fc639b56bc19f17f.png?width=140&height=140"
+    },
+    {
+      id: 3,
+      title: "Mobiles",
+      "name": "Mi 10T Pro",
+      "price": "₹ 36,999",
+      "image": "https://i02.appmifile.com/406_operator_in/15/10/2020/4481bf3c3bb2ce53d9f4ac6ce1d7e312.png?width=140&height=140"
+    },
+    {
+      id: 4,
+      title: "Mobiles",
+      "name": "Mi 10T",
+      "price": "from ₹ 32,999",
+      "image": "https://prasadyash2411.github.io/ecom-website/img/Album%203.png"
+    },
+    {
+      id: 5,
+      title: "Mobiles",
+      "name": "Mi Notebook 14(IC)",
+      "price": "From ₹ 43,999",
+      "image": "https://i02.appmifile.com/763_operator_in/19/01/2021/4be770942f3352ad2e7e3a94e5d75a43.jpg?width=140&height=140"
+    },
+    {
+      id: 6,
+      title: "Laptops",
+      "name": "Mi Notebook 14",
+      "price": "From ₹ 41,999",
+      "image": "https://i01.appmifile.com/webfile/globalimg/in/cms/9534EF41-E7D9-025C-A287-B4BAFCA4A0F9.jpg?width=140&height=140"
+    },
+    {
+      id: 7,
+      title: "Laptops",
+      "name": "Mi NoteBook 14 e-Learning Edition",
+      "price": "₹ 38,999",
+      "image": "https://i02.appmifile.com/234_operator_in/05/11/2020/5b641713340b4a542b75cf737ed2cd80.png?width=140&height=140"
+    },
+    {
+      id: 8,
+      title: "Laptops",
+      "name": "Mi Webcam HD",
+      "price": "₹ 1,099",
+      "image": "https://i02.appmifile.com/504_operator_in/05/01/2021/d465ac7bca989e33487b4b4f2447c3ea.png?width=140&height=140"
+    },
+    {
+      id: 9,
+      title: "Others",
+      "name": "Mi Watch Revolve",
+      "price": "₹ 9,999",
+      "image": "https://i02.appmifile.com/669_operator_in/29/09/2020/492027bef78ed4c69cfd68c2d299d49f.png?width=140&height=140"
+    },
+    {
+      id: 10,
+      title: "Laptops",
+      "name": "Redmi Watch GPS",
+      "price": "₹ 3,999",
+      "image": "https://i02.appmifile.com/594_operator_in/13/05/2021/7a6d0c6dfdc36b8e65a210c6e40e5171.jpg?width=140&height=140"
+    },
+    {
+      id: 11,
+      "name": "Mi Beard Trimmer 1C",
+      "price": "₹ 999",
+      "image": "https://i02.appmifile.com/979_operator_in/06/08/2020/855d6e62a97cb56339126c68de3918e9.png?width=140&height=140"
+    },
+    {
+      id: 12,
+      title: "Laptops",
+      "name": "Mi Athleisure Shoes",
+      "price": "₹ 1,699",
+      "image": "https://i02.appmifile.com/205_operator_in/29/09/2020/e989b9be493148ba6461e8b43d4df81d.png?width=140&height=140"
+    },
+  ];
+  
+  export default list;
